8d54ba250aa5ab76278503371213eb4bcf938b65
evalparse <zhuojia.dai@gmail.com>
2020-06-16 11:01:40 +1000

    Update add-compiled.jl

--- add-compiled.jl
+++ add-compiled.jl
@@ -1,3 +1,3 @@
 using Pkg
-pkg"add https://github.com/KristofferC/PackageCompilerX.jl.git CategoricalArrays Clustering CSV DataFrames DataFramesMeta DecisionTree FastGroupBy Feather FileIO FreqTables GLM JDF JLBoost Lazy Missings ParquetFiles RDatasets SortingLab StatsBase Tables Plots StatsPlots Pipe Fread FstFileFormat"
+pkg"add https://github.com/KristofferC/PackageCompilerX.jl.git CategoricalArrays Clustering CSV DataFrames DataFramesMeta DecisionTree FastGroupBy Feather FileIO FreqTables GLM JDF JLBoost Lazy Missings Parquet RDatasets SortingLab StatsBase Tables Plots StatsPlots Pipe Fread FstFileFormat"
 pkg"precompile"
